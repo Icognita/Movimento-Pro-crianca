5440ba29c8b07b2a7e54dd615f6f737662aaa21e
Mari Mendes <mendes.mariannedev@gmail.com>
2023-12-05 22:11:37 -0300

    renderizando o formulario

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-const express= require('express')
-const ejs = require('ejs');
- const app =express()
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+PORT = 5656;
 
- //conexao com o bando
-const db = require('./db/models/index')
+//conexao com o bando
+const db = require("./db/models/index");
 
- app.set('view engine', "ejs"); 
+app.set("view engine", "ejs");
 
- app.listen(5656,()=>{
-    console.log("servidor rodando na porta5656");
- })
+app.get("/mpc/cadastroAluno", function (req, res) {
+  res.render("../src/view/page/formCadastroAluno.ejs");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando no endereco http://localhost:${PORT}/mpc`);
+});
