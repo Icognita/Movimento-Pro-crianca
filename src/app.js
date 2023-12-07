028390b8fc26d49d7e9bb169b6e98d7a4668f743
Pedro Vitor <pedrovitor812.812@gmail.com>
2023-12-07 10:53:00 -0300

    Iniciando routes e controllers e configurando server

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
-const express= require('express')
+const express= require('express');
 const ejs = require('ejs');
- const app =express()
-
- //conexao com o bando
+const bodyParser = require('body-parser');
 const db = require('./db/models');
+const routes = require('./routes/routes')
+const app =express();
+const PORT = 5656
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(routes);
 
- app.set('view engine', "ejs"); 
+app.set('view engine', 'ejs'); 
 
- app.listen(5656,()=>{
-    console.log("servidor rodando na porta 5656");
- })
+app.listen(PORT,()=>{
+   console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ src/controller/alunoController.js
@@ -0,0 +1,5 @@
+class AlunoController {
+    async createAluno(req, res){
+        
+    }
+};
