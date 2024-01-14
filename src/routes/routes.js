bb43ed203deb8c54bef486e433b1d87ab4ddbaaf
Pedro Vitor <pedrovitor812.812@gmail.com>
2024-01-14 19:44:27 -0300

    Adição do middleware na rota post de /cadastrar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const alunoController = require('../controller/alunoController')
 const controllerUsuario=require('../controller/User')
+const validarCadastroAluno = require('../middlewares/validacoes_cadastro_aluno')
 
 //usuario
 router.post('/Criar/Usuario',controllerUsuario.CriarUsuario)
-router.post('/cadastrar', alunoController.criarAluno)  // Rota de cadastro
+router.post('/cadastrar', validarCadastroAluno, alunoController.criarAluno)  // Rota de cadastro
 router.get('/cadastrar', alunoController.telaCadastro)
+
 
 module.exports = router
