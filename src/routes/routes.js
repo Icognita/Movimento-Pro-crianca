bf66039f9e2d19915442297d093a1f1194e7c593
Icognita <mirianvir@hotmail.com>
2024-01-17 17:41:03 -0300

    merge e atualização das rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,30 +3,30 @@ const router = express.Router();
 const alunoController = require('../controller/alunoController')
 const controllerUsuario=require('../controller/User')
 const controllerLogin=require('../controller/login')
+const routes = express.Router();
 
 const validarCadastroAluno = require('../middlewares/validacoes_cadastro_aluno')
 
 
 //login
 router.post('/Criar/Usuario',controllerUsuario.CriarUsuario)
-router.post('/cadastrar', validarCadastroAluno, alunoController.criarAluno)  // Rota de cadastro
+// router.post('/cadastrar', validarCadastroAluno, alunoController.criarAluno)  // Rota de cadastro
 router.get('/cadastrar', alunoController.telaCadastro)
 router.post('/login',controllerLogin.login)
 //verificar autenticação
 
-router.get()
-module.exports=router;
-const routes = express.Router();
 
-routes.get("/teste", (req, res) => {
+
+
+routes.get("/home", (req, res) => {
     res.send("Funcionado ...")
 })
-const validarCadastroAluno = require('../middlewares/validacoes_cadastro_aluno')
+
 
 //usuario
 router.post('/Criar/Usuario',controllerUsuario.CriarUsuario)
-router.post('/cadastrar', validarCadastroAluno, alunoController.criarAluno)  // Rota de cadastro
 router.get('/cadastrar', alunoController.telaCadastro)
+router.post('/cadastrar', validarCadastroAluno, alunoController.criarAluno)  // Rota de cadastro
 
 
 module.exports = router
